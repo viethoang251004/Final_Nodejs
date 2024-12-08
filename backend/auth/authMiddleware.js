20c9a9ff85a4027247335e41801c7024f242d784
Hoang Nguyen <viethoang251004@gmail.com>
2024-12-08 23:27:09 +0700

    Update backend/auth/authMiddleware.js

--- backend/auth/authMiddleware.js
+++ backend/auth/authMiddleware.js
@@ -3,16 +3,23 @@ const UserModel = require('../models/UserModel');
 
 const authMiddleware = async (req, res, next) => {
     try {
+        // Lấy token từ cookie
         const token = req.cookies.userDataLogin;
+
+        // Nếu token tồn tại, kiểm tra tính hợp lệ và giải mã nó
         if (token) {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await UserModel.findById(decoded.id).select(
-                'name email',
-            );
+
+            // Tìm thông tin người dùng trong cơ sở dữ liệu dựa trên ID từ token
+            // Chỉ lấy các trường 'name' và 'email'
+            req.user = await UserModel.findById(decoded.id).select('name email');
         }
     } catch (error) {
+        // Nếu có lỗi, đặt giá trị `req.user` thành null
         req.user = null;
     }
+
+    // Chuyển sang middleware hoặc route handler tiếp theo
     next();
 };
 
