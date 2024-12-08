5534af2f1122251e4eb501eb03e6222f67753ea2
Hoang Nguyen <viethoang251004@gmail.com>
2024-12-08 23:30:36 +0700

    Update backend/auth/CheckAdminAccess.js

--- backend/auth/CheckAdminAccess.js
+++ backend/auth/CheckAdminAccess.js
@@ -2,13 +2,6 @@ module.exports = async (req, res, next) => {
     try {
         // const userDataLogin = req.cookies.userDataLogin;
         const isAdmin = req.cookies.userRole;
-        // console.log('userData retrieved from cookie:', userDataLogin);
-
-        // if (!userDataLogin) {
-        //     return res.status(401).json({ code: 401, message: 'Token không tìm thấy' });
-        // }
-
-        // console.log(isAdmin)
 
         // Check user permissions
         if (isAdmin.localeCompare('ADMIN') === 0) {
